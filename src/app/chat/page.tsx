51ce70c637edeaa38e0f6341daaf1e63aafdb1c6
Lynxpetal <92204026+Lynxpetal@users.noreply.github.com>
2023-11-03 01:52:56 +0800

    changes

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,19 +1,22 @@
 "use client"
-import TextareaAutosizeProps  from "react-textarea-autosize";
+import TextareaAutosizeProps from "react-textarea-autosize";
+import { FC, HTMLAttributes, useState } from "react";
 
 export default function chat() {
+    const [input, setInput] = useState<string>('')
 
-    
     return (
         <div className="flex flex-col pl-1 p-4 min-h-screen relative">
             <div className="flex sm:pl-6 xl:pl-16 absolute bottom-3 w-full">
-            <TextareaAutosizeProps
-            rows={2}
-            maxRows={4}
-            autoFocus
-            placeholder="Write something here!"
-            className="peer disabled:opacity-50 resize-none block border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6 w-11/12 rounded" />
-</div>
-        </div>
+                <TextareaAutosizeProps
+                    rows={2}
+                    maxRows={4}
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    autoFocus
+                    placeholder="Write something here!"
+                    className="peer disabled:opacity-50 resize-none block border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6 w-11/12 rounded" />
+            </div>
+        </div> //39.50
     )
 }
