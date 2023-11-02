d1a777693ae2beefde300318e83b6b328c670422
Lynxpetal <92204026+Lynxpetal@users.noreply.github.com>
2023-11-02 14:40:22 +0800

    init chat

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,12 +1,18 @@
 "use client"
-
+import ReactTextareaAutosize from "react-textarea-autosize";
+import { TextareaAutosizeProps } from "react-textarea-autosize";
 
 export default function Home() {
 
     
     return (
         <div className="flex flex-col items-center justify-between p-24">
-            
+            <ReactTextareaAutosize
+            rows={2}
+            maxRows={4}
+            autoFocus
+            placeholder="Write something here!"
+            className="" //continue 32.22
         </div>
     )
 }
