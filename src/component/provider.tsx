c1b525f9d889b94301df763a4bf31fe804272d1b
Lynxpetal <92204026+Lynxpetal@users.noreply.github.com>
2023-11-03 12:17:09 +0800

    installed tanstack

---
+++ src/component/provider.tsx
@@ -0,0 +1,13 @@
+"use client"
+import {  } from "@tanstack";
+import { FC } from "react";
+
+interface ProvidersProps {}
+
+const Providers: FC<ProvidersProps> = ({}) => {
+    const queryClient = new Query
+    
+    return
+}
+
+export default Providers
