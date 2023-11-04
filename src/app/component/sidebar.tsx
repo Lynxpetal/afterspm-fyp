6f29826fc994abfe2bad4ace9bcbdf7a1a2414a1
Lynxpetal <92204026+Lynxpetal@users.noreply.github.com>
2023-11-05 00:20:10 +0800

    SIDEBAR

--- src/app/component/sidebar.tsx
+++ src/app/component/sidebar.tsx
@@ -25,7 +25,7 @@ export default function compSidebar() {
     //   }, []);
 
     useEffect(() => {
-        setLogin("user")
+        setLogin("admin")
         //add listener to listen login or logout
     }, []);
 
@@ -37,49 +37,43 @@ export default function compSidebar() {
     }
 
     return (
-        <Sidebar aria-label="Sidebar with logo branding example">
+        <Sidebar aria-label="Sidebar with logo branding example" className="w-full">
             <Sidebar.Logo href="#" img="/vercel.svg" imgAlt="Flowbite logo">
                 AfterSPM
             </Sidebar.Logo>
             <Sidebar.Items>
-                <Sidebar.ItemGroup>
+                {isLogin === "admin" ? <Sidebar.ItemGroup>
                     <Sidebar.Item href="/" icon={HiChartPie}>
                         Home
                     </Sidebar.Item>
-                    {isLogin === "admin" ? <Sidebar.Item href="/instituteAdmin" icon={HiUser}>
+                    <Sidebar.Item href="/instituteAdmin" icon={HiUser}>
                         Institute Info
-                    </Sidebar.Item> : <Sidebar.Collapse icon={HiShoppingBag} label="Career Reccomend" disabled={isLogin === "guest"}>
+                    </Sidebar.Item>
+                    <Sidebar.Item href="/Prog" icon={HiUser}>
+                        Manage Programme<br />Information
+                    </Sidebar.Item>
+                </Sidebar.ItemGroup> : <Sidebar.ItemGroup>
+                    <Sidebar.Item href="/" icon={HiChartPie}>
+                        Home
+                    </Sidebar.Item>
+                    <Sidebar.Collapse icon={HiShoppingBag} label="Career Reccomend" disabled={isLogin === "guest"}>
                         <Sidebar.Item href="./Career/Holland">1. Holland's Test</Sidebar.Item>
                         <Sidebar.Item href=".">2. Big Five Test</Sidebar.Item>
                         <Sidebar.Item href="#">3. Test Result</Sidebar.Item>
                         <Sidebar.Item href="#">4. Recommendation</Sidebar.Item>
                     </Sidebar.Collapse>
-                    }
-
-                    {isLogin === "admin" ? <Sidebar.Item href="/Prog" icon={HiUser}>
-                        Manage Programme<br />Information
-                        </Sidebar.Item> :
-                        <Sidebar.Item href="/chat" icon={HiViewBoards} disabled={isLogin === "guest"}>
-                            Chat
-                        </Sidebar.Item>
-                    }
-
-                    
-                        <Sidebar.Item href="#" icon={HiInbox} disabled={isLogin === "guest"}>
-                            Course Recommend
-                        </Sidebar.Item>
-                    
-
-                    
-                        <Sidebar.Item href="#" icon={HiUser} disabled={isLogin === "guest"}>
-                            Institute Filter
-                        </Sidebar.Item>
-                    
-
-
-                </Sidebar.ItemGroup>
+                    <Sidebar.Item href="/chat" icon={HiViewBoards} disabled={isLogin === "guest"}>
+                        Chat
+                    </Sidebar.Item>
+                    <Sidebar.Item href="#" icon={HiInbox} disabled={isLogin === "guest"}>
+                        Course Recommend
+                    </Sidebar.Item>
+                    <Sidebar.Item href="#" icon={HiUser} disabled={isLogin === "guest"}>
+                        Institute Filter
+                    </Sidebar.Item>
+                </Sidebar.ItemGroup>}
                 <Sidebar.ItemGroup>
-                    {isLogin ? <Sidebar.Item href="/login" icon={HiArrowSmRight}>
+                    {isLogin === "guest" ? <Sidebar.Item href="/login" icon={HiArrowSmRight}>
                         Sign In
                     </Sidebar.Item> : <Sidebar.Item href="#" icon={HiArrowSmRight} onClick={logout}>
                         Sign Out
@@ -87,7 +81,6 @@ export default function compSidebar() {
                     <Sidebar.Item href="/register" icon={HiArrowSmRight} onClick={logout}>
                         Register
                     </Sidebar.Item>
-
                 </Sidebar.ItemGroup>
             </Sidebar.Items>
         </Sidebar>
