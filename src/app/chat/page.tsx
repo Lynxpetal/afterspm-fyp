efd6751d0d8daea954e2142f219baf95f8569ec3
Lynxpetal <92204026+Lynxpetal@users.noreply.github.com>
2023-11-07 01:05:31 +0800

    chatGPT progress

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,15 +3,23 @@ import { Message } from "@/app/lib/validators/message";
 import { useMutation } from "@tanstack/react-query";
 import { nanoid } from "nanoid";
 import TextareaAutosizeProps from "react-textarea-autosize";
-import { FC, HTMLAttributes, useState } from "react";
+import { FC, HTMLAttributes, useContext, useRef, useState } from "react";
 import ChatMessages from "../component/ChatMessages";
+import { MessageContext } from "../context/messages";
 
 interface ChatInputProps extends HTMLAttributes<HTMLDivElement>{}
 
 const ChatInput: FC<ChatInputProps> = ({}) => {
-
     const [input, setInput] = useState<string>('')
-
+    const {
+        messages,
+        addMessage,
+        removeMessage,
+        updateMessage,
+        setIsMessageUpdating,
+    } = useContext(MessageContext)
+    const textareaRef = useRef<null | HTMLTextAreaElement>(null)
+    
     const {mutate: sendMessage} = useMutation({
         mutationKey: ['sendMessage'],
         mutationFn: async (messages: Message) => {
@@ -24,8 +32,23 @@ const ChatInput: FC<ChatInputProps> = ({}) => {
             })
             return response.body
         },
+        onMutate(message){
+            addMessage(message)
+        },
         onSuccess: async (stream) => {
             if (!stream) throw new Error("No stream found")
+
+            const id = nanoid()
+            const responseMessage: Message = {
+                id, 
+                isUserMessage:false,
+                text: '',
+            }
+
+            addMessage(responseMessage)
+
+            setIsMessageUpdating(true)
+
 
             const reader = stream.getReader()
             const decoder = new TextDecoder()
@@ -35,8 +58,15 @@ const ChatInput: FC<ChatInputProps> = ({}) => {
                 const {value, done: doneReading} = await reader.read()
                 done = doneReading
                 const chunkValue = decoder.decode(value)
-                console.log(chunkValue)
+                updateMessage(id, (prev) => prev + chunkValue)
             }
+
+            setIsMessageUpdating(false)
+            setInput('')
+
+            setTimeout(() => {
+                textareaRef.current?.focus()
+            }, 10)
         },
     })
     return (
@@ -44,6 +74,7 @@ const ChatInput: FC<ChatInputProps> = ({}) => {
             <ChatMessages className="px-2 py-3"/>
             <div className="flex sm:pl-6 xl:pl-16 absolute bottom-3 w-full">
                 <TextareaAutosizeProps
+                    ref={textareaRef}
                     rows={2}
                     onKeyDownCapture={(e) => {
                         if (e.key === 'Enter' && !e.shiftKey) {
--- src/app/component/provider.tsx
+++ src/app/component/provider.tsx
@@ -1,16 +1,19 @@
 "use client"
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { FC, ReactNode } from "react";
+import { MessagesProvider } from "../context/messages";
 
 interface ProvidersProps {
     children: ReactNode
 }
 
-const Providers: FC<ProvidersProps> = ({children}) => {
+const Providers: FC<ProvidersProps> = ({ children }) => {
     const queryClient = new QueryClient()
 
-    
-    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+
+    return <QueryClientProvider client={queryClient}>
+        <MessagesProvider>{children}</MessagesProvider>
+    </QueryClientProvider>
 }
 
 export default Providers
