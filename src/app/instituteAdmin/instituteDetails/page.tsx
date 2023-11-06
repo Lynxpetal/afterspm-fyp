b3f9153f5783ae95044e8555a082c92d9ca5ec24
Calvin <mokckc-wm20@student.tarc.edu.my>
2023-11-06 10:15:15 +0800

    Update (View Institute)

--- src/app/instituteAdmin/instituteDetails/page.tsx
+++ src/app/instituteAdmin/instituteDetails/page.tsx
@@ -1,14 +1,16 @@
 'use client'
-import React, { useEffect, useState } from "react";
-import { useSearchParams } from 'next/navigation';
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
-import { db } from "../../FirebaseConfig/firebaseConfig";
-import { Button, Label, TextInput, FileInput } from 'flowbite-react'
+import React, { useEffect, useState } from "react"
+import { useSearchParams } from 'next/navigation'
+import { doc, getDoc } from "firebase/firestore"
+import { db } from "../../FirebaseConfig/firebaseConfig"
+import {  Label, TextInput, Kbd } from 'flowbite-react'
 import { BiSolidUser } from 'react-icons/bi'
 import { ImLocation2 } from 'react-icons/im'
 import { BsFillTelephoneFill } from 'react-icons/bs'
 import { HiMail } from 'react-icons/hi'
-import MoonLoader from "react-spinners/MoonLoader";
+import MoonLoader from "react-spinners/MoonLoader"
+import { BiArrowBack } from "react-icons/bi"
+import Link from "next/link"
 
 export default function InstituteDetails() {
   //retrieve the id
@@ -20,67 +22,128 @@ export default function InstituteDetails() {
   //const getHotel = doc(firestore,  `Institute/${instituteId}`)
   const [isInstituteFetchDataLoading, setIsInstituteFetchDataLoading] = useState(true)
   const [institute, setInstitute] = useState({})
+  const [instituteImageUrl, setInstituteImageUrl] = useState('')
+  const [instituteName, setInstituteName] = useState('')
+  const [instituteLocation, setInstituteLocation] = useState('')
+  const [institutePhoneNumber, setInstitutePhoneNumber] = useState('')
+  const [instituteEmailAddress, setInstituteEmailAddress] = useState('')
 
   const fetchInstituteData = async () => {
     //data is fetching = loading
     setIsInstituteFetchDataLoading(true);
-  
+
     try {
       const instituteDocRef = doc(db, "Institute", instituteId);
       const instituteDocSnap = await getDoc(instituteDocRef);
-  
+
       if (instituteDocSnap.exists()) {
         console.log("Document data: ", instituteDocSnap.data());
         setInstitute(instituteDocSnap.data());
+        setInstituteName(instituteDocSnap.data().InstituteName)
+        setInstituteLocation(instituteDocSnap.data().InstituteLocation)
+        setInstitutePhoneNumber(instituteDocSnap.data().InstitutePhoneNumber)
+        setInstituteEmailAddress(instituteDocSnap.data().InstituteEmailAddress)
+        setInstituteImageUrl(instituteDocSnap.data().InstituteImageUrl)
+        console.log(instituteImageUrl)
       } else {
         console.log("No document");
       }
     } catch (error) {
       console.error("Error fetching document: ", error);
-      
+
       //data is fetched successfully
     } finally {
       setIsInstituteFetchDataLoading(false);
     }
   };
-  
+
 
   useEffect(() => {
     fetchInstituteData();
   }, [])
 
-  if (isInstituteFetchDataLoading) 
-  return <div className="grid">
-    <MoonLoader
-      loading={isInstituteFetchDataLoading}
-      size={50}
-      color="#8DD3E2"
-      
-    />
-    <h1>Loading...</h1>
-  </div>
+  if (isInstituteFetchDataLoading)
+    return <div className="grid">
+      <MoonLoader
+        loading={isInstituteFetchDataLoading}
+        size={50}
+        color="#8DD3E2"
+
+      />
+      <h1>Loading...</h1>
+    </div>
   return (
-    <form className="flex max-w-md flex-col gap-4" style={{ margin: '20px', padding: '20px', backgroundColor: '#EDFDFF', width:"500px" }} noValidate>
-      <h1 className="loginHeader">View Institute Information</h1>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="name" value="Name" />
+    <div className="card" style={{ margin: '30px' }}>
+      <div style={{ backgroundColor: "#EDFDFF", margin: '30px', padding: '30px', width: '75%' }}>
+        <div>
+          <Link href ={{pathname: '/instituteAdmin'}}>
+            <Kbd icon={BiArrowBack} />
+          </Link>
+        </div>
+        <h1 className="loginHeader">View Institute Information</h1>
+        <div style={{ paddingBottom: '20px' }}>
+          <div className="mb-2 block">
+            <Label htmlFor="name" value="Name" />
+          </div>
           <TextInput
             type="text"
             className="form-control"
             id="name"
             icon={BiSolidUser}
-            placeholder="Tunku Abdul Rahman University of Management and Technology"
-            value={institute.InstituteName}
-            disabled
+            value={instituteName}
           />
         </div>
+
+        <div style={{ paddingBottom: '20px' }}>
+          <div className="mb-2 block">
+            <Label htmlFor="location" value="Location" />
+          </div>
+          <TextInput
+            type="text"
+            className="form-control"
+            id="location"
+            icon={ImLocation2}
+            value={instituteLocation}
+          />
+        </div>
+
+        <div style={{ paddingBottom: '20px' }}>
+          <div className="mb-2 block">
+            <Label htmlFor="phone" value="Phone Number" />
+          </div>
+          <TextInput
+            type="tel"
+            className="form-control"
+            id="phoneNumber"
+            icon={BsFillTelephoneFill}
+            value={institutePhoneNumber}
+          />
+        </div>
+
+        <div style={{ paddingBottom: '20px' }}>
+          <div className="mb-2 block">
+            <Label htmlFor="email" value="Email Address" />
+          </div>
+          <TextInput
+            type="tel"
+            className="form-control"
+            id="emailAddress"
+            icon={HiMail}
+            value={instituteEmailAddress}
+          />
+        </div>
+
+        <div style={{ paddingBottom: '20px' }}>
+          <div id="fileUpload" className="max-w-md">
+            <div className="mb-2 block">
+              <Label htmlFor="file" value="Image" />
+            </div>
+            <img src={instituteImageUrl} height="auto" width="auto" />
+          </div>
+        </div>
+
       </div>
-
-
-
-
-    </form>
+    </div>
   )
 
 }
