829ef823abee02f7c4c00292b8ef3a6ce8d52e90
Lynxpetal <92204026+Lynxpetal@users.noreply.github.com>
2023-11-12 20:05:35 +0800

    .

--- src/app/component/PsychForms.tsx
+++ src/app/component/PsychForms.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { FC, HTMLAttributes, useState } from "react";
 import { cn } from "../lib/validators/utils";
-import { Label, Radio } from 'flowbite-react';
+import { Carousel, Label, Pagination, Radio } from 'flowbite-react';
 import { Question } from "../lib/validators/message";
 
 
@@ -12,7 +12,8 @@ interface FormProps extends HTMLAttributes<HTMLDivElement> {
 
 const PsychForm: FC<FormProps> = ({ className, Form, Title, ...props }) => {
     const [CurrentForm, setCurrentForm] = useState<Question[]>(Form)
-
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const onPageChange = (page: number) => setCurrentPage(page);
     function getLabel(value: number) {
         switch (value) {
             case 1:
@@ -30,7 +31,26 @@ const PsychForm: FC<FormProps> = ({ className, Form, Title, ...props }) => {
                 break;
         }
     }
+    function pageDisplay() {
+        return <fieldset className="flex max-w-md pl-11 flex-col gap-4">
+            <legend className="mb-4">{CurrentForm[currentPage].label}</legend>
+            {CurrentForm[currentPage].options.map((option) => {
+                if (option == 3) {
+                    return <div className="flex items-center gap-2">
+                        <Radio id="united-state" name={CurrentForm[currentPage].label} value={option} defaultChecked />
+                        <Label htmlFor="united-state">
+                            {getLabel(option)}</Label>
+                    </div>
+                }
+                return <div className="flex items-center gap-2">
+                    <Radio id="united-state" name={CurrentForm[currentPage].label} value={option} />
+                    <Label htmlFor="united-state">
+                        {getLabel(option)}</Label>
+                </div>
+            })}
+        </fieldset>
 
+    }
     return (
         <div
             {...props}
@@ -38,29 +58,15 @@ const PsychForm: FC<FormProps> = ({ className, Form, Title, ...props }) => {
                 '',
                 className
             )}>
-            <fieldset className="flex max-w-md flex-col gap-4">
-                <legend className="mb-4 text-xl">{Title}</legend>
-                {
-                    CurrentForm.map((questions) => {
-                        return <fieldset className="flex max-w-md flex-col gap-4">
-                            <legend className="mb-4">{questions.label}</legend>
-                            {questions.options.map((option) => {
-                                if (option == 3) {
-                                    return <div className="flex items-center gap-2">
-                                    <Radio id="united-state" name={questions.label} value={option} defaultChecked />
-                                    <Label htmlFor="united-state">
-                                        {getLabel(option)}</Label>
-                                </div>
-                                }
-                                return <div className="flex items-center gap-2">
-                                    <Radio id="united-state" name={questions.label} value={option} />
-                                    <Label htmlFor="united-state">
-                                        {getLabel(option)}</Label>
-                                </div>
-                            })}
-                        </fieldset>
-                    })}
-            </fieldset >
+            <div className="flex flex-col gap-60">
+                <fieldset className="flex items-center mt-8 py-10 max-w-md flex-col gap-4 shadow-md rounded bg-white">
+                    <legend className="m-3 p-3 text-2xl font-semibold antialiased bg-slate-50 shadow-md rounded-md">{Title}</legend>
+                    {pageDisplay()}
+                </fieldset >
+                <div className="flex overflow-x-auto sm:justify-center">
+                    <Pagination layout="navigation" currentPage={currentPage} totalPages={CurrentForm.length} onPageChange={onPageChange} showIcons />
+                </div>
+            </div>
         </div >
     )
 }
