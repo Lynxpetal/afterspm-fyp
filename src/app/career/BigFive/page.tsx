2ad6a7ff4f056d82e8b5d0e9af39836c77cb96ff
Lynxpetal <92204026+Lynxpetal@users.noreply.github.com>
2023-11-16 19:14:15 +0800

    .

---
+++ src/app/career/BigFive/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { FC, HTMLAttributes } from "react";
+import PsychForm from "@/app/component/PsychForms";
+import holland from "@/json/hollands.json";
+import { Question } from "@/app/lib/validators/message";
+
+interface ChatInputProps extends HTMLAttributes<HTMLDivElement> { }
+
+
+const ChatInput: FC<ChatInputProps> = ({ }) => {
+    const hollandForm: Question[] =  holland.content
+    
+    return (
+        <div className={"flex flex-col min-h-screen "}>
+            <div className="p-10 m-6 bg-slate-100">
+            <PsychForm Title="Big Five Personality Test" Form={hollandForm} />
+            </div>
+        </div>
+    )
+}
+
+export default ChatInput
