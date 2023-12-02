0147c2be9a1e3181b1c4cd5af4aca153a3d61fc5
Calvin <mokckc-wm20@student.tarc.edu.my>
2023-12-02 10:46:35 +0800

    Update abit

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -28,8 +28,8 @@ export default function Register() {
     return value === values.password || "Passwords do not match"
   }
 
-  const isRegisterEmailValid =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-  
+  const isRegisterEmailValid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+
   //if want have the color
   const getEmailColor = () => {
     if (errors[0]?.email) {
@@ -43,7 +43,7 @@ export default function Register() {
 
   useEffect(() => {
     const isPasswordValid = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-    const isEmailValid =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    const isEmailValid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 
     if (isEmailValid.test(email)) {
       form.clearErrors("0.email")
@@ -60,110 +60,120 @@ export default function Register() {
   const createAccount = (data: [RegisterFormValues]) => {
     console.log("Ok", data)
     createUserWithEmailAndPassword(authRegister, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user
-      sendEmailVerification(user)
-      alert("Registered successfully. Please check your email address for verification")
-      //Will decide where to go
-      router.push("/login")
-    })
-    .catch((error) => {
-      const errorCode = error.errorCode
-      alert(errorCode)
-    })
+      .then((userCredential) => {
+        const user = userCredential.user
+        sendEmailVerification(user)
+        alert("Registered successfully. Please check your email address for verification")
+        //Will decide where to go
+        router.push("/login")
+      })
+      .catch((error) => {
+        const errorCode = error.errorCode
+        alert(errorCode)
+      })
   }
 
   return (
-    <form className="flex max-w-md flex-col gap-4" style={{ margin: '20px' }} onSubmit={handleSubmit(createAccount)} noValidate>
-      <h1 className="registerHeader">Register</h1>
-      <h1 className="registerDescription">Create an account and enjoy it</h1>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="email" value="Email address" />
+    <form style={{ margin: '20px' }} onSubmit={handleSubmit(createAccount)} noValidate>
+      <div className="card" style={{ margin: '30px' }}>
+        <div style={{ backgroundColor: "#EDFDFF", margin: '30px', padding: '30px', width: '50%' }}>
+          <h1 className="registerHeader">Register</h1>
+          <h1 className="registerDescription">Create an account and enjoy it</h1>
+          <div style={{ paddingBottom: '20px' }}>
+            <div className="mb-2 block">
+              <Label htmlFor="email" value="Email address" />
+            </div>
+            <TextInput
+              type="email"
+              className="form-control"
+              //color={getEmailColor()}
+              id="email"
+              icon={HiMail}
+              placeholder="abc@gmail.com"
+              {...register("0.email", {
+                required: {
+                  value: true,
+                  message: "Email is required"
+                },
+                pattern: {
+                  value: isRegisterEmailValid,
+                  message: "Invalid email format. Follow the format: abc@gmail.com"
+                }
+              })}
+              onChange={(e) => setRegisterEmail(e.target.value)}
+              helperText={
+                <>
+                  We'll never share your details. Read our
+                  <a href="/register/privacyPolicy" className="ml-1 font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                    Privacy Policy
+                  </a>
+                  .
+                </>
+              }
+            />
+            <p className="registerValidationError">{errors[0]?.email?.message}</p>
+          </div>
+
+          <div style={{ paddingBottom: '20px' }}>
+            <div className="mb-2 block">
+              <Label htmlFor="password" value="Password" />
+            </div>
+            <TextInput
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="*******"
+              autoComplete='off'
+              {...register('0.password', {
+                required: {
+                  value: true,
+                  message: "Password is required"
+                },
+                pattern: {
+                  value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                  message: "Password must meet the criteria: at least 8 characters long including at least  one uppercase letter, one lowercase letter, one digit, and one special character"
+                },
+              })}
+              onChange={(e) => setRegisterPassword(e.target.value)}
+            />
+            <p className="registerValidationError">{errors[0]?.password?.message}</p>
+          </div>
+
+          <div style={{ paddingBottom: '20px' }}>
+            <div className="mb-2 block">
+              <Label htmlFor="confirmPassword" value="Confirm password" />
+            </div>
+            <TextInput
+              type="password"
+              className="form-control"
+              id="confirmPassword"
+              placeholder="*******"
+              autoComplete='off'
+              {...register('0.confirmPassword', {
+                required: {
+                  value: true,
+                  message: "Confirm password is required"
+                },
+                validate: {
+                  passwordMatch: (value) => validatePasswordMatch(value, { password }),
+                },
+              })}
+              onChange={(e) => setRegisterConfirmPassword(e.target.value)}
+            />
+            <p className="registerValidationError">{errors[0]?.confirmPassword?.message}</p>
+          </div>
+
+          <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: '10px' }}>
+            <Button type="submit">Register</Button>
+          </div>
+
+          <h2 className="haveAnAccountHeader">Have an account?
+            <a href="/login" className="ml-1 font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+              Login
+            </a>
+          </h2>
         </div>
-        <TextInput
-          type="email"
-          className="form-control"
-          //color={getEmailColor()}
-          id="email"
-          icon={HiMail}
-          placeholder="abc@gmail.com"
-          {...register("0.email", {
-            required: {
-              value: true,
-              message: "Email is required"
-            },
-            pattern: {
-              value: isRegisterEmailValid,
-              message: "Invalid email format. Follow the format: abc@gmail.com"
-            }
-          })}
-          onChange={(e) => setRegisterEmail(e.target.value)}
-          helperText={
-            <>
-              We'll never share your details. Read our
-              <a href="#" className="ml-1 font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Privacy Policy
-              </a>
-              .
-            </>
-          }
-        />
-        <p className="registerValidationError">{errors[0]?.email?.message}</p>
       </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="password" value="Password" />
-        </div>
-        <TextInput
-          type="password"
-          className="form-control"
-          id="password"
-          placeholder="*******"
-          autoComplete='off'
-          {...register('0.password', {
-            required: {
-              value: true,
-              message: "Password is required"
-            },
-            pattern: {
-              value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-              message: "Password must meet the criteria: at least 8 characters long including at least  one uppercase letter, one lowercase letter, one digit, and one special character"
-            },
-          })}
-          onChange={(e) => setRegisterPassword(e.target.value)}
-        />
-        <p className="registerValidationError">{errors[0]?.password?.message}</p>
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="confirmPassword" value="Confirm password" />
-        </div>
-        <TextInput
-          type="password"
-          className="form-control"
-          id="confirmPassword"
-          placeholder="*******"
-          autoComplete='off'
-          {...register('0.confirmPassword', {
-            required: {
-              value: true,
-              message: "Confirm password is required"
-            },
-            validate: {
-              passwordMatch: (value) => validatePasswordMatch(value, { password }),
-            },
-          })}
-          onChange={(e) => setRegisterConfirmPassword(e.target.value)}
-        />
-        <p className="registerValidationError">{errors[0]?.confirmPassword?.message}</p>
-      </div>
-      <Button type="submit">Register</Button>
-      <h2 className="haveAnAccountHeader">Have an account?
-        <a href="/login" className="ml-1 font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-          Login
-        </a>
-      </h2>
     </form>
   )
 }
---
+++ src/app/register/privacyPolicy/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+export default function privacyPolicy() {
+
+  return (
+    <div>
+      <form style={{ margin: '20px' }}>
+        <div className="card" style={{ margin: '30px' }}>
+          <div style={{ backgroundColor: "#EDFDFF", margin: '30px', padding: '30px', width: '75%' }}>
+          <h1 style={{color: "black"}}>Ok</h1>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
